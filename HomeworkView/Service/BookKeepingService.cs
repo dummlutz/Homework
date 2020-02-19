84586f4b5d203838e310a453c81ffe2788aeca0f
黃政綸 <allen@kingfor.com.tw>
2020-02-20 01:24:54 +0800

    change load data function

--- HomeworkView/Service/BookKeepingService.cs
+++ HomeworkView/Service/BookKeepingService.cs
@@ -13,17 +13,35 @@ namespace HomeworkView.Service
 
         public static List<BookKeepingViewModel> GetData()
         {
-            var query = from q in _HomeworkDbContext.AccountBook
-                        select new BookKeepingViewModel
-                        {
-                            ID = q.Id.ToString(),
-                            Amout=q.Amounttt,
-                            Category= q.Categoryyy.ToString(),
-                            CreateDate = q.Dateee,
-                            Remark = q.Remarkkk,
-                        };
+            var data = _HomeworkDbContext.AccountBook.OrderByDescending(x => x.Dateee);
+            var list = new List<BookKeepingViewModel>();
+            foreach (var item in data)
+            {
+                list.Add(new BookKeepingViewModel
+                {
+                    ID = item.Id.ToString(),
+                    Amout = item.Amounttt,
+                    Category = ((CategoryEnum)item.Categoryyy).GetDescription(),
+                    CreateDate = item.Dateee,
+                    Remark = item.Remarkkk,
+                });
+            }
+            return list;
+
+            //var query = from q in _HomeworkDbContext.AccountBook
+            //            select new BookKeepingViewModel
+            //            {
+            //                ID = q.Id.ToString(),
+            //                Amout=q.Amounttt,
+            //                Category = q.Categoryyy.ToString(),
+            //                CreateDate = q.Dateee,
+            //                Remark = q.Remarkkk,
+            //            };
+
             // ((CategoryEnum)q.Categoryyy).GetDescription(),
-            return query.ToList();
+            //(CategoryEnum)Enum.ToObject(typeof(CategoryEnum), q.Categoryyy).GetDescription(),
+            //return query.ToList();
         }
+
     }
 }
